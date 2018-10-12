4cb76e225a00cf6d7b7c8712ef8d5c6674d42431
Lewis Cruz <bluelew75@gmail.com>
2018-10-12 15:34:33 -0400

    Created controllers for Admin, Authors, Books, and Sales.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,10 @@
+class AuthorsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'sales/index'
+  get 'sales/show'
+  get 'sales/new'
+  get 'sales/create'
+  get 'authors/index'
+  get 'authors/show'
+  get 'authors/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
