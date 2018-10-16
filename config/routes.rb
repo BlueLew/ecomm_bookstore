7e855884257ec544e626fb8c9e1633f1c8c73dab
Lewis Cruz <bluelew75@gmail.com>
2018-10-16 16:36:54 -0400

    Added Styleguide view and edite Seed file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'styleguide/index'
   devise_for :users
   get 'users/show'
   get 'users/update'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
-User.create!(name: "Lewis", email: "lewis@bookstore.dev", password: "testing1")
+# User.create!(name: "Lewis", email: "lewis@bookstore.dev", password: "testing1")
+
+# cs_lewis = User.create!(name: "C.S. Lewis", email: "cslewis@cslewis.dev", password: "testing1")
+
+books = [
+  { title: "The Screwtape Letters", author_id: 1, price: 10.99 },
+  { title: "Mere Christianity", author_id: 1, price: 9.89 },
+  { title: "The Problem of Pain", author_id: 1, price: 11.89 },
+  { title: "The Lion, the Witch, and the Wardrobe", author_id: 1, price: 16.99 }
+]
+books.each do |book|
+  Book.create!(title: book[:title], price: book[:price])
+end
 
 # Examples:
 #
