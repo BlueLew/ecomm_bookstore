476b01930937b92f1aeacba3b1cdc7ef1343fc21
Lewis Cruz <bluelew75@gmail.com>
2018-10-18 11:56:49 -0400

    Added Action Mailer

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -14,6 +14,7 @@ class SalesController < ApplicationController
     stripe_token = sale_params[:stripe_token]
     stripe_charge = StripeServices::CreateCharge.call(@book, current_user, stripe_token)
     Sale.create(book: @book, user: current_user, stripe_charge_id: stripe_charge.id)
+    PurchaseMailer.with(user: current_user).thank_you.deliver_later
     redirect_to sales_path
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,8 @@ Rails.application.configure do
 
   # Store files locally.
   config.active_storage.service = :local
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
