be4674886c0cdac26ee11b3d54efe53f58ef85be
Lewis Cruz <bluelew75@gmail.com>
2018-10-23 21:07:08 -0400

    Added pagination to authors index.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,11 @@ class AuthorsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @authors = User.where(author: true)
+    #@authors = User.where(author: true)
+    @current_page = params[:page] || 1
+    @authors = User.where(author: true) && User.page(@current_page)
+    @prev_page = User.page(@current_page).prev_page
+    @next_page = User.page(@current_page).next_page
   end
 
   def show
